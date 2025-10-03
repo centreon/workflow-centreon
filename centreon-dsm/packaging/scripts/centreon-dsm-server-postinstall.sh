acece5b055aeee58adf12970e0895acca07eedee
schapron <schapron@centreon.com>
2025-10-03 16:26:16 +0200

    ci(secu): clean the repo

--- centreon-dsm/packaging/scripts/centreon-dsm-server-postinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-systemctl daemon-reload ||:
-systemctl unmask dsmd.service ||:
-systemctl preset dsmd.service ||:
-systemctl enable dsmd.service ||:
-systemctl restart dsmd.service ||:
--- centreon/packages/ui/cypress/support/component.tsx
+++
@@ -1,6 +0,0 @@
-import './commands';
-import '@cypress/code-coverage/support';
-
-import enableVisualTesting from '@centreon/js-config/cypress/component/enableVisualTesting';
-
-enableVisualTesting();
